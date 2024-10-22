618d5273c84c59c41b348376589bca68cde8eb87
Adari <adari.adari@capgemini.com>
2024-10-22 15:45:26 +0530

    Initial commit

---
+++ src/main/java/TestProj/TestProjApplication.java
@@ -0,0 +1,13 @@
+package TestProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Test/TestProj/TestProjApplicationTests.java
@@ -0,0 +1,13 @@
+package Test.TestProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
